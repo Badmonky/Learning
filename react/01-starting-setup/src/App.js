2fe88a5c6d4e6d50fef6fb5f6825057172c0c15d
Badmonky <hermann.sven.o@gmail.com>
2022-11-25 19:50:54 -0700

    adding data and passing through props

--- react/01-starting-setup/src/App.js
+++ react/01-starting-setup/src/App.js
@@ -1,12 +1,14 @@
 import ExpenseItem from "./components/ExpenseItem";
+import expenses from "./components/expenses.js";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      {expenses.map((expense) => {
+        return <ExpenseItem expense={expense}></ExpenseItem>;
+      })}
     </div>
-    
   );
 }
 
--- react/01-starting-setup/src/components/ExpenseItem.js
+++ react/01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,27 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+// import expenses from "./expenses.js"
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const expenseMonth = props.expense.date.toLocaleString("en-US", {
+    month: "long",
+  });
+  const expenseDay = props.expense.date.toLocaleString("en-US", {
+    day: "2-digit",
+  });
+  const expenseYear = props.expense.date.getFullYear();
+  const expenseTitle = props.expense.title;
+  const expenseAmount = props.expense.amount;
+
   return (
-    <div className='expense-item'>
-      <div>March 28th 2022</div>
-      <div className='expense-item__description'>
-        <h2 className='h2'>Car Insurance</h2>
-        <div className='expense-item__price'>$248.67</div>
+    <div className="expense-item">
+      <div>
+        <div>{expenseMonth}</div>
+        <div>{expenseDay}</div>
+        <div>{expenseYear}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
