68fbbf037ae81e9573952c9252a9d9f596540214
Badmonky <hermann.sven.o@gmail.com>
2022-11-25 20:21:49 -0700

    splitting components

--- react/01-starting-setup/src/components/ExpenseItem.js
+++ react/01-starting-setup/src/components/ExpenseItem.js
@@ -1,24 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-// import expenses from "./expenses.js"
 
 function ExpenseItem(props) {
-  const expenseMonth = props.expense.date.toLocaleString("en-US", {
-    month: "long",
-  });
-  const expenseDay = props.expense.date.toLocaleString("en-US", {
-    day: "2-digit",
-  });
-  const expenseYear = props.expense.date.getFullYear();
   const expenseTitle = props.expense.title;
   const expenseAmount = props.expense.amount;
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{expenseMonth}</div>
-        <div>{expenseDay}</div>
-        <div>{expenseYear}</div>
-      </div>
+      <ExpenseDate date={props.expense.date}/>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
