0e35ff354a8147f8e4b3e1608f2f3f018ab91fbb
Badmonky <hermann.sven.o@gmail.com>
2022-11-26 09:32:15 -0700

    adding children props and card component

--- react/01-starting-setup/src/components/ExpenseItem.js
+++ react/01-starting-setup/src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   const expenseTitle = props.expense.title;
   const expenseAmount = props.expense.amount;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.expense.date}/>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- react/01-starting-setup/src/components/expenses.js
+++ react/01-starting-setup/src/components/expenses.js
@@ -1,13 +1,15 @@
 import ExpenseItem from "./ExpenseItem";
 import expenseData from "./data.js";
+import './Expenses.css'
+import Card from "./Card";
 
 function Expenses() {
   return (
-    <div>
+    <Card className='expenses'>
       {expenseData.map((expense) => {
         return <ExpenseItem expense={expense} />;
       })}
-    </div>
+    </Card>
   );
 }
 
